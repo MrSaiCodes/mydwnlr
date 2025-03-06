6897314fe2f134ee0003a8f8e24c908abfd398cc
MrSaiCodes <saiwebsite4us@gmail.com>
2025-03-06 20:33:31 +0530

    vars.py

--- vars.py
+++ vars.py
@@ -1,5 +1,5 @@
-api_id = 28080069 #change_with_your_Api_id.
-api_hash = "d64b54aaaea703aea09607889cf76e98" #change_with_your_Api_hash.
+api_id = 28062363 #change_with_your_Api_id.
+api_hash = "c1cf86ea7f575612a8bfae5ccc7b1dc5" #change_with_your_Api_hash.
 bot_token = "7610010893:AAF7YRZRvaGMLSeeIihcdkyJGRtYoQMVmxY" #change_with_your_bot_token.
 auth_users = [662494886] #change_with_your_user_id.  
 sudo_user = 662494886 #change_with_your_user_id.
